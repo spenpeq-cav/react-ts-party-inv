91bab5d42b672791e0f57ca8eb5e9056990bcc1a
Spencer Pequegnat <spencer.peq@gmail.com>
2022-01-03 19:14:02 -0800

    AddToList form component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import List from "./components/List";
+import AddToList from "./components/AddToList";
 
-interface IState {
+export interface IState {
   people: {
     name: string;
     age: number;
@@ -26,6 +27,7 @@ function App() {
     <div className="App">
       <h1>People Invited to the Party</h1>
       <List people={people} />
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
